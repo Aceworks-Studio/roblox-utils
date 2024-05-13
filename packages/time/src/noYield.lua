21e055244aa39ed1585c5be877530f6f7f4a13d6
jeparlefrancais <jeparlefrancais21@gmail.com>
2024-05-13 10:26:13 -0400

    Add noYield function
    `noYield` makes sure that the given function will not yield during its
    execution. If it does, it will throw an error if dev mode is enabled
    (using the _G.DEV global variable)

---
+++ packages/time/src/noYield.lua
@@ -0,0 +1,28 @@
+local function noYield<T..., R...>(fn: (T...) -> R..., ...: T...): R...
+    if _G.DEV then
+        local thread = coroutine.create(fn)
+
+        local results = table.pack(coroutine.resume(thread, ...))
+
+        local success = results[1]
+        if not success then
+            local err = results[2]
+
+            local message = if typeof(err) == 'string'
+                then debug.traceback(thread, err)
+                else tostring(err)
+
+            error(message, 2)
+        end
+
+        if coroutine.status(thread) ~= 'dead' then
+            error(debug.traceback(thread, 'function is not allowed to yield'), 2)
+        end
+
+        return table.unpack(results :: any, 2)
+    else
+        return fn(...)
+    end
+end
+
+return noYield
