0dcd3b148907cdc6dbbf094c865a3efee7bddef5
jeparlefrancais <jeparlefrancais21@gmail.com>
2024-05-23 12:22:18 -0400

    add loopWhile to init.lua

--- packages/time/src/init.lua
+++ packages/time/src/init.lua
@@ -1,5 +1,6 @@
 local debounce = require('./debounce')
 local loopUntil = require('./loopUntil')
+local loopWhile = require('./loopWhile')
 local noYield = require('./noYield')
 local throttle = require('./throttle')
 local yieldUntil = require('./yieldUntil')
@@ -9,6 +10,7 @@ export type Interval = loopUntil.Interval
 return {
     debounce = debounce,
     loopUntil = loopUntil,
+    loopWhile = loopWhile,
     noYield = noYield,
     throttle = throttle,
     yieldUntil = yieldUntil,
