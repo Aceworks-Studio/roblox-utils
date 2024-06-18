737167743493ce5f5176d566ec449974ee474fa6
jeparlefrancais <jeparlefrancais21@gmail.com>
2024-06-18 09:58:51 -0400

    flip boolean chances

--- packages/random/src/values/boolean.lua
+++ packages/random/src/values/boolean.lua
@@ -2,7 +2,7 @@ export type BooleanGenerator = (chance: number?) -> boolean
 
 local function createBooleanGenerator(random: Random): BooleanGenerator
     local function randomBool(chance: number?): boolean
-        return random:NextNumber() >= (chance or 0.5)
+        return random:NextNumber() <= (chance or 0.5)
     end
 
     return randomBool
